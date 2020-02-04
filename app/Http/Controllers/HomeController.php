4ecd9cf45cbda736a4d1a37a458107cd1464e5b8
Jerico Reynaldi <jerico.reynaldi@edi-indonesia.local>
2020-02-04 12:35:44 +0700

    perubahan pertama

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+      $users = [];
+        return view('home', compact('users'));
     }
 }
